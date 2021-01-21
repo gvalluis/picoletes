37cd2e47af7225d21f2e1b272cdf209aad446ea2
Gabriel Valluis <gabriel.valluis@rarolabs.com.br>
2021-01-21 14:42:20 -0300

    Solution; Primeiros Models;

---
+++ Picoletes/Picoletes.Dominio/Models/Vendedor.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Picoletes.Dominio.Models
+{
+    public class Vendedor
+    {
+        public string Nome { get; set; }
+    }
+}
