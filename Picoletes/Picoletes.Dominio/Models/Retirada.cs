8a4a4931ee19d7e4b600d92e1c605a696e7c953c
Gabriel Valluis <gabriel.valluis@rarolabs.com.br>
2021-01-21 21:17:02 -0300

    Estruturação da interface; Inicio do processo;

---
+++ Picoletes/Picoletes.Dominio/Models/Retirada.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Picoletes.Dominio.Models
+{
+    public class Retirada
+    {
+        public string Id { get; set; }
+        public string DescricaoProduto { get; set; }
+        public int QtdEstoque { get; set; }
+        public int QtdRetirada { get; set; }
+        public int ValidadeForaFreezer { get; set; }
+    }
+}
---
+++ Picoletes/Picoletes.Interface/Form1.cs
@@ -0,0 +1,38 @@
+﻿using Picoletes.Dominio.Services;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Picoletes.Interface
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void ExecutarRetiradaClick(object sender, EventArgs e)
+        {
+
+            MessageBox.Show("Retirada de produtos realizada com sucesso", "Retirada",
+            MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+
+        }
+
+        private void GerarRetiradaClick(object sender, EventArgs e)
+        {
+            RetiradaService retiradaService = new RetiradaService();
+            retiradaService.GerarRetirada();
+            MessageBox.Show("Arquivo de retirada gerado", "Sucesso",
+            MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+    }
+}
