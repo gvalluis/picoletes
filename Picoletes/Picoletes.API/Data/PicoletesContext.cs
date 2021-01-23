75b7bd0cd0bf62023148a8702b825c3471b13a82
Gabriel Valluis <gabriel.valluis@rarolabs.com.br>
2021-01-23 12:05:47 -0300

    Configuração de banco de dados; Adição de dlls;

---
+++ Picoletes/Picoletes.API/Data/PicoletesContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Picoletes.API.Models;
+
+namespace Picoletes.API.Data
+{
+    public class PicoletesContext : DbContext
+    {
+        public PicoletesContext(DbContextOptions<PicoletesContext> options) : base(options)
+        {
+        }
+
+        public DbSet<PedidoRetirada> PedidoRetirada { get; set; }
+
+    }
+}
--- Picoletes/Picoletes.API/Startup.cs
+++ Picoletes/Picoletes.API/Startup.cs
@@ -1,8 +1,10 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Picoletes.API.Data;
 using Picoletes.API.Interfaces;
 using Picoletes.API.Repository;
 using Picoletes.API.Services;
@@ -11,12 +13,11 @@ namespace Picoletes.API
 {
     public class Startup
     {
+        private readonly IConfiguration _configuration;
         public Startup(IConfiguration configuration)
         {
-            Configuration = configuration;
+            _configuration = configuration;
         }
-
-        public IConfiguration Configuration { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
@@ -24,6 +25,8 @@ namespace Picoletes.API
             services.AddControllers();
             services.AddScoped<IRetiradaRepository, RetiradaRepository>();
             services.AddScoped<IRetiradaService, RetiradaService>();
+            services.AddDbContext<PicoletesContext>(x => 
+                x.UseSqlite(_configuration.GetConnectionString("DefaultConnection")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
